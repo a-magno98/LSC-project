fef097542d053bc20059f6ddb0126d3f2792ccce
a-magno98 <80479517+a-magno98@users.noreply.github.com>
2022-09-22 16:12:10 +0200

    Add files via upload

---
+++ sparkkafka.py
@@ -0,0 +1,40 @@
+#version direct approach
+import sys
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="WordCountKafka")
+    ssc = StreamingContext(sc, 2)
+    brokers, topic = sys.argv[1:]
+    kvs = KafkaUtils.createDirectStream(ssc, [topic],{"metadata.broker.list": brokers})
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(" ")) \
+                  .map(lambda word: (word, 1)) \
+                  .reduceByKey(lambda a, b: a+b)
+    counts.pprint()
+    ssc.start()
+    ssc.awaitTermination()
+    
+#version reciever-based approach
+import sys
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="WordCountKafka")
+    ssc = StreamingContext(sc, 2)
+    broker, topic = sys.argv[1:]
+    kvs = KafkaUtils.createStream(ssc, \
+                                  broker, \
+                                  "raw-event-streaming-consumer", \
+                                  {topic:1}) 
+    lines = kvs.map(lambda x: x[1])
+    counts = lines.flatMap(lambda line: line.split(" ")) \
+                  .map(lambda word: (word, 1)) \
+                  .reduceByKey(lambda a, b: a+b)
+    counts.pprint()
+    ssc.start()
+    ssc.awaitTermination()
